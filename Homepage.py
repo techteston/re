7cbdddf30e12c5bfde738e9243ee3eac23e35fb9
techteston <techteston@gmail.com>
2023-08-21 12:00:01 +0530

    Update
    Update

--- Homepage.py
+++ Homepage.py
@@ -13,6 +13,18 @@ footer {visibility: hidden;}
 """
 st.markdown(hide_st_style,unsafe_allow_html=True)
 
-st.title("The Produce Marketplace")
+st.title("The :green[Produce] Marketplace")
 st.subheader('A Marketplace for Grocers, Retailers and Farmers')
+
+choice = st.selectbox('Login/SignUp',['Log In','Sign Up'])
+if choice == "Log In":
+    usrid = st.text_input('User ID')
+    paswd = st.text_input('Password',type='password')
+    st.button('Log In')
+else:
+    usrid = st.text_input('User ID')
+    paswd = st.text_input('Password',type='password')
+    st.button('Sign Up')
+
+st.write('Selection',choice)
 
